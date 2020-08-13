ea06e3780434b80fa3fa0b6ab65939be8e3f37c5
Myeonghwan Kim <Myeonghwan@korea.omti.com>
2020-08-14 08:58:59 +0900

    Edit DivisibleSumPairs & Add MigratoryBirds

--- Algorithm/DivisibleSumPairs.cs
+++ Algorithm/DivisibleSumPairs.cs
@@ -8,5 +8,24 @@ namespace Algorithm
 {
 	class DivisibleSumPairs
 	{
+
+		public int divisibleSumPairs(int n, int k, int [] ar)
+		{
+			int result = 0;
+
+			for (int i = 0; i < n - 1; i++)
+			{
+				for (int j = 1; j < n - i; j++)
+				{
+					if( (ar[i] + ar[i+j]) % k == 0)
+					{
+						result++;
+					}
+				}
+			}
+
+			return result;
+		}
+
 	}
 }
--- Algorithm/MigratoryBirds.cs
+++ Algorithm/MigratoryBirds.cs
@@ -8,5 +8,54 @@ namespace Algorithm
 {
 	class MigratoryBirds
 	{
+		public int migratoryBirds(List<int> arr)
+		{
+			int highest = 0;
+
+			int num1 = 0;
+			int num2 = 0;
+			int num3 = 0;
+			int num4 = 0;
+			int num5 = 0;
+
+			for(int i = 0; i<arr.Count; i++)
+			{
+				switch (arr[i])
+				{
+					case 1:
+						num1++;
+						break;
+					case 2:
+						num2++;
+						break;
+					case 3:
+						num3++;
+						break;
+					case 4:
+						num4++;
+						break;
+					case 5:
+						num5++;
+						break;
+					default:
+						break;
+				}
+			}
+
+			List<int> frequency = new List<int> { num1, num2, num3, num4, num5 };
+			int highestNum = 0;
+
+			for(int i = 0; i<frequency.Count; i++)
+			{
+				if(highest < frequency[i])
+				{
+					highest = frequency[i];
+					highestNum = i + 1;
+				}
+
+			}
+
+			return highestNum;
+		}
 	}
 }
